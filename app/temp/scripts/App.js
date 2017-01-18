d48815ae9dd9dd568916c53ca871cf8dbe977f28
Rafal Rudzinski <rrudzin1@asu.edu>
2017-01-18 11:35:15 -0800

    added padding to svg to remove artifacts in icons

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11220,7 +11220,7 @@
 	    _createClass(StickyHeader, [{
 	        key: 'refreshWaypoints',
 	        value: function refreshWaypoints() {
-	            this.lazyImages.load(function () {
+	            this.lazyImages.on('load', function () {
 	                Waypoint.refreshAll();
 	            });
 	        }
